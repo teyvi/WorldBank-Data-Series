f370d270c5383703d66db5a90a630c3c2da0f71f
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-02-03 23:54:15 +0000

    -m

---
+++ electricity_access_by_population.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from pandas_datareader import wb
+import geopandas
+
+electric_access = wb.download(country='all', indicator='EG.ELC.ACCS.ZS', start=2000, end= 2020)
+electric_access = electric_access.reset_index(1)
+electric_access.columns = ['Year', 'Electricity Access']
+print(electric_access)
+
+world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
+pd.set_option('display.max_columns', 10)
+pd.set_option('display.max_columns', 10)
+pd.set_option('display.width', 1000)
+print(world)
+
