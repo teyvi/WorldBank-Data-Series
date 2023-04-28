e79485e808fbcd26f6edb28d67e117dec5faee95
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-04-28 17:50:26 +0000

    inflation in Africa project

---
+++ inflation_africa.py
@@ -0,0 +1,6 @@
+from pandas_datareader import wb
+
+data=wb.download(indicator='FP.CPI.TOTL.ZG',
+                 country= ('DZ', 'AO', 'BJ', 'BW', 'BF', 'BI', 'CM', 'CV', 'CF', 'TD', 'KM', 'CD', 'CG', 'CI', 'DJ', 'EG', 'GQ', 'ER', 'ET', 'GA', 'GM', 'GH', 'GN', 'GW', 'KE', 'LS', 'LR', 'LY', 'MG', 'MW', 'ML', 'MR', 'MU', 'MA', 'MZ', 'NA', 'NE', 'NG', 'RW', 'ST', 'SN', 'SC', 'SL', 'SO', 'ZA', 'SS', 'SD', 'SZ', 'TZ', 'TG', 'TN', 'UG', 'ZM', 'ZW')
+                 ,start=2000, end=2020)
+print(data)
