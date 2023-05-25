4efe135c59cae7007431d5bc1bdbc26d5285f97c
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-05-25 01:57:49 +0000

    merging data

--- timeseries_data.py
+++ timeseries_data.py
@@ -28,31 +28,26 @@ africa_map.replace("Côte d'Ivoire" , "Cote d'Ivoire", inplace = True)
 africa_map.replace("Democratic Republic of the Congo","Congo, Dem. Rep.", inplace = True)
 africa_map.replace("Republic of Congo","Congo, Rep.", inplace = True)
 africa_map.replace("Cape Verde","Cabo Verde", inplace = True)
-#africa_map.replace("Egypt, Arab Rep.","Egypt", inplace = True)
-#africa_map.replace("Gambia, The","Gambia", inplace = True)
-#africa_map.replace("Equatorial Guinea","Eq. Guinea", inplace = True) y
-#africa_map.replace("Mauritius","Eq. Guinea", inplace = True)
-#africa_map.replace("South Sudan","S. Sudan", inplace = True) y
-#africa_map.replace("Sao Tome and Principe","S. Sudan", inplace = True)
-#africa_map.replace("Eswatini","eSwatini", inplace = True) y
-#africa_map.replace["Seychelles","Djibouti", inplace = True)
+africa_map.replace("Egypt","Egypt, Arab Rep.", inplace = True)
+africa_map.replace("Gambia" , "Gambia, The", inplace = True)
+africa_map.replace("Swaziland","Eswatini", inplace = True)
 
-
-
-
+merge= africa_map.join(data, on = "NAME_0" , how = 'right')
+print(merge)
 #expanding the table
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.expand_frame_repr', False)
 
 #data cleaning- checking data for similar entries
-for index, row in data.iterrows():
-    if index not in africa_map['NAME_0'].to_list():
-        print(index + ' is not in the list' )
-    else:
-        pass
+
+#for index, row in data.iterrows():
+#    if index not in africa_map['NAME_0'].to_list():
+#        print(index + ' is not in the list' )
+#   else:
+#      pass
 
 #printing results
-print(africa_map)
 #print(africa_map)
-print(data)
+#print(africa_map)
+#print(data)
