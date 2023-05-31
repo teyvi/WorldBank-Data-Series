3f31c5fa7a16ee03d5bf68b2cd21f46006c729fd
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-05-31 19:22:45 +0000

    plotting map

--- timeseries_data.py
+++ timeseries_data.py
@@ -2,21 +2,23 @@ import geopandas as gpd
 import pandas as pd
 from pandas_datareader import wb
 import matplotlib.pyplot as plt
+import mapclassify
+
+from inflation_africa import ax
 
 # downloading data for only Africa
-data=wb.download(indicator='FP.CPI.TOTL.ZG',
-                 country=("DZ", "AO", "BJ", "BW", "BF", "BI", "CV", "CM", "CF", "TD",
-                          "KM", "CG", "CD", "CI", "DJ", "EG", "GQ", "ER", "SZ", "ET",
-                          "GA", "GM", "GH", "GN", "GW", "KE", "LS", "LR", "LY", "MG",
-                          "MW", "ML", "MR", "MU", "MA", "MZ", "NA", "NE", "NG", "RW",
-                          "ST", "SN", "SC", "SL", "SO", "ZA", "SS", "SD", "TZ", "TG",
-                          "TN", "UG", "ZM", "ZW")
-                 , start=2000, end=2020)
+data= wb.download(indicator='FP.CPI.TOTL.ZG',
+                 country =("DZ", "AO", "BJ", "BW", "BF", "BI", "CV", "CM", "CF", "TD",
+                           "KM", "CG", "CD", "CI", "DJ", "EG", "GQ", "ER", "SZ", "ET",
+                           "GA", "GM", "GH", "GN", "GW", "KE", "LS", "LR", "LY", "MG",
+                           "MW", "ML", "MR", "MU", "MA", "MZ", "NA", "NE", "NG", "RW",
+                           "ST", "SN", "SC", "SL", "SO", "ZA", "SS", "SD", "TZ", "TG",
+                           "TN", "UG", "ZM", "ZW"),
+                  start=2000, end=2020)
 
 # resetting index for merging
 data = data.reset_index(1)
 data.columns = ['year', 'inflation']
-pd.set_option('display.max_rows', None)
 
 # Reading the world map
 africa_map = gpd.read_file(r'/Users/angelateyvi/Documents/GitHub/WorldBank-Data-Series/worldmap/african_map.shp')
@@ -32,6 +34,8 @@ africa_map.replace("Swaziland", "Eswatini", inplace = True)
 
 # merging the data with africa map
 merge = africa_map.join(data, on = "NAME_0" , how = 'right')
+
+#itereate each column of year to add to title
 # expanding the table
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
@@ -39,16 +43,30 @@ pd.set_option('display.expand_frame_repr', False)
 print(merge.head())
 
 # plot
-merge.plot(column= 'inflation');
+merge.plot(column= 'year',
+           cmap= 'Reds',
+           legend = True,
+           scheme = 'user_defined',
+           classification_kwds = {'bins' :[-10, 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+                                           110, 120, 130, 140, 150, 160, 170, 180, 190, 200,
+                                           210, 220, 230, 240, 250, 260, 270, 280, 290, 300,
+                                           310, 320, 330, 340, 350, 360, 370, 380, 390, 400,
+                                           410, 420, 430, 440, 450, 460, 470, 480, 490, 500,
+                                           510, 520, 530, 540, 550, 560, 570, 580, 590, 600]
+                                  }
+           )
+
+#plt.show()
 
 # adding a title to the map
-# ax.set_title ('Inflation in Countries at')
+ax.set_title ('Inflation in Countries at', fontdict =
+{'fontsize':20},pad = 12.5)
 
 # removing the axis
-# ax.set_axis_off()
+ax.set_axis_off()
 
 # move legend
-# ax.get_legend().set_bbox_to_anchor= ((0.18,0.6))
+ax.get_legend().set_bbox_to_anchor= ((0.18, 0.6))
 
 
 # data cleaning- checking data for similar entries
